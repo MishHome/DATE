118eba70d169a1720d8f150205930c85171518d2
MishHome <ostapchuk.mish@yandex.ru>
2023-08-16 11:21:47 +0300

    Add project files.

---
+++ DATE/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GetOldArePerson;
+class Person
+{
+    public string Name { get; set; }
+    public DateOnly DateOfBird { get; set; }
+    public string City { get; set; }
+    public string Telephone { get; set; }
+
+    public static List<Person> GetPersons()
+    {
+        var p1 = new Person() { Name = "Jon", DateOfBird = new DateOnly(2000, 1, 1), City = "Moscow" };
+        var p2 = new Person() { Name = "Time", DateOfBird = new DateOnly(1980, 12, 31), City = "Kostroma" };
+        var p3 = new Person() { Name = "Micle", DateOfBird = new DateOnly(2011, 01, 31), City = "Moscow" };
+
+        return new List<Person> { p1, p2, p3 };
+    }
+
+}
+
---
+++ DATE/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace GetOldArePerson;
+class Program
+{
+    public static void Main()
+    {
+        var listPersons = new List<Person>();
+        //var listPersons = Person.GetPersons();
+
+        Console.WriteLine("Name");
+        var name = Console.ReadLine();
+        Console.WriteLine("DateOfBird yy-MM-dd");
+        var dateOfBird = Console.ReadLine();
+        Console.WriteLine("City");
+        var city = Console.ReadLine();
+
+        Person person1= new Person();
+        person1.Name = name;
+        person1.City = city;
+        person1.DateOfBird = DateOnly.ParseExact(dateOfBird,"yy-MM-dd");
+        
+        listPersons.Add(person1);
+
+        foreach (var person in listPersons)
+        {
+            var OldArePerson = GetOldArePerson(person.DateOfBird);
+
+            Console.WriteLine($"Name: {person.Name}\nDateOfBird: {person.DateOfBird}\nOldArePerson: {OldArePerson}");
+            Console.WriteLine("---");
+        }
+        Console.ReadLine();
+    }
+
+    static int? GetOldArePerson(DateOnly dateOfBird)
+    {
+        var DateToday = DateOnly.FromDateTime(DateTime.Now);
+        if (dateOfBird > DateToday)
+        {
+            Console.WriteLine($"дата рождения не может быть больше сегоднешней даты");
+            return null;
+        }
+        else
+        {
+            var fullYear = DateToday.Year - dateOfBird.Year;
+
+            if (DateToday.Month < dateOfBird.Month)
+                fullYear--;
+            else if
+                (DateToday.Month == dateOfBird.Month && DateToday.Day < dateOfBird.Day)
+                fullYear--;
+
+            return fullYear;
+        }
+    }
+}
+
+
+
